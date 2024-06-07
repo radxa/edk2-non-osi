33eb6069f88b2dc9aecc039f79166ee0af854a7b
ReleaseRaptor <ReleaseRaptor@cixtech.com>
2024-06-07 18:31:28 +0800

    Milestone: P1 Beta1 RELEASE
    Update to P1 Beta1 RELEASE build of commit e164865eff3c
    
    1.Support Dual boot for Linux and Windows
    2.Support RTC wakeup
    3.Add Secure Boot Function(default is disabled)
    4.UEFI setup menu construction update.
    5.Support 32GB memory board
    6.enable boot logo

---
+++ Platform/CIX/Readme.md
@@ -0,0 +1,46 @@
+CIX EVB Platform
+=======================================
+
+# Summary
+
+This is a port of ARM64 Tiano Core UEFI firmware for the CIX EVB platform based on the CIX P1 SoC.
+
+CIX P1 edk2 code is base on as follows:
+- [edk2](https://github.com/tianocore/edk2): `fb493ac84ebc6860e1690770fb88183effadebfb`
+- [edk2-platforms](https://github.com/tianocore/edk2-platforms): `8ea6ec38da8812f0703e8845fe639b8845704f96`
+
+# How to build (X86 Linux Environment)
+  1. Install Arm GNU Toolchain on X86 machines.
+    Download AArch64 bare-metal target (aarch64-none-elf) for x86_64 Linux hosted cross toolchains from https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads. The verified version is 10.2-2020.11
+    
+  2. Config your GCC tool in build_and_package.sh
+    For Example:
+    
+    $ export ARM_TOOLCHAIN_ELF="gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf"
+    
+  3. Install ACPI Tool
+    Download ACPICA tool from https://github.com/acpica/acpica.git. The verifed tag is R03_31_22
+    
+  4. Config your ACPI tool in build_and_package.sh
+    For Example:
+    
+    $ export IASL_PREFIX="${WORKSPACE}/tools/acpica/generate/unix/bin/"
+
+  5. Config your edk2 submodule update method in build_and_package.sh
+
+  6. Create symbolic link for build_and_package.sh and run it
+    For Example:
+
+    $ cd $YOUR_WORKSPACE
+    $ ln -s edk2-non-osi/Platform/CIX/Sky1/PackageTool/build_and_package.sh build_and_package.sh
+    $ ./build_and_package.sh
+    
+  7. Found "cix_flash_all.bin" and "SKY1_BL33_UEFI.fd" in output folder
+
+# How to Flash Firmware
+  1. Use SPI Flash Programmer(like DediProg SF100) by flash file "cix_flash_all.bin"
+
+  2. Run FlashUpdate.efi(edk2-non-osi/Platform/CIX/Sky1/FlashTool/FlashUpdate.efi) under UEFI shell
+    For Example:
+
+    FS0:\>FlashUpdate.efi -f cix_flash_all.bin
---
+++ Platform/CIX/Sky1/PackageTool/build_and_package.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+export  BOLD="\e[1m"
+export  NORMAL="\e[0m"
+export	RED="\e[31m"
+export	GREEN="\e[32m"
+export	YELLOW="\e[33m"
+export  BLUE="\e[94m"
+export  CYAN="\e[36m"
+
+export WORKSPACE=$PWD
+export PATH_OUT="${WORKSPACE}/output"
+
+export ARM_TOOLCHAIN_ELF="gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf"
+export UEFI_PROJECT="Merak"
+export UEFI_DSC_FILE="Platform/CIX/Sky1/${UEFI_PROJECT}/${UEFI_PROJECT}.dsc"
+export GCC5_AARCH64_PREFIX="${WORKSPACE}/tools/gcc/${ARM_TOOLCHAIN_ELF}/bin/aarch64-none-elf-"
+export IASL_PREFIX="${WORKSPACE}/tools/acpica/generate/unix/bin/"
+export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
+
+
+exec_build_uefi() {
+    
+if [ ! -e $WORKSPACE/edk2/MdeModulePkg/Library/BrotliCustomDecompressLib/brotli/.git ]; then
+	echo "Need update edk2 submodule!"
+	cd $WORKSPACE/edk2
+	git submodule init
+	git config --list|grep submodule.*url=|sed -e 's#^\(.*url\)=https://github.com/\(.*\)$#git config \1 ssh://git@gitmirror.cixcomputing.com/github_mirror/\2##g'|while read c;do $c;done
+	git submodule update --init
+fi
+
+cd $WORKSPACE/edk2-platforms
+local COMMIT_HASH=`git rev-parse --short=12 HEAD`
+
+cd $WORKSPACE
+
+if [ ! -e $WORKSPACE/Source/C/bin ]; then
+	echo "Need build edk2 basetool!"
+	make -C edk2/BaseTools
+fi
+
+if [ ! -e $WORKSPACE/tools/acpica/generate/unix/bin ]; then
+	echo "Need build acpi tool!"
+	make -C tools/acpica
+fi
+
+source $WORKSPACE/edk2/edksetup.sh --reconfig
+
+rm -rf ${WORKSPACE}/Build
+
+local BUILD_DATE=`date +%VM%y%m%d%H%M%SN`
+local UEFI_TARGET=RELEASE
+local BOARD=evb
+build -a AARCH64 -t GCC5 -p $UEFI_DSC_FILE -b $UEFI_TARGET -D BOARD_NAME=$BOARD -D BUILD_DATE=$BUILD_DATE -D COMMIT_HASH=$COMMIT_HASH -D SMP_ENABLE=1 -D ACPI_BOOT_ENABLE=1
+
+cp Build/${UEFI_PROJECT}/${UEFI_TARGET}_GCC5/FV/SKY1_BL33_UEFI.fd ${PATH_OUT}
+
+}
+
+exec_cix_mkimage() {
+    export PATH_PACKAGE_TOOL="${WORKSPACE}/edk2-non-osi/Platform/CIX/Sky1/PackageTool"
+    local PATH_FIRMARES="${PATH_OUT}/Firmwares"
+    local PATH_KEYS="${PATH_OUT}/Keys"
+
+    # copy require files to output
+    cp -r "${PATH_PACKAGE_TOOL}/Firmwares/" "${PATH_OUT}"
+
+    cp -r "${PATH_PACKAGE_TOOL}/Keys/" "${PATH_OUT}"
+
+    # check bootloader1 image
+    if [[ ! -e "${PATH_FIRMARES}/bootloader1.img" ]]; then
+        echo "ERROR: no file ${PATH_FIRMARES}/bootloader1.img"
+        exit 1
+    fi
+
+    # Generate bootloader2 image
+    cd "${PATH_OUT}"
+
+    "${PATH_PACKAGE_TOOL}/cix_image_tool.sh" -p -K Keys -T rsa3072 --bf31 "${PATH_FIRMARES}/tf-a.bin" --bf32 "${PATH_FIRMARES}/tee.bin" --bf33 "${PATH_OUT}/SKY1_BL33_UEFI.fd" -o "${PATH_FIRMARES}/bootloader2.img"
+
+    cd -
+    if [[ ! -e "${PATH_FIRMARES}/bootloader2.img" ]]; then
+        echo "ERROR: no file ${PATH_FIRMARES}/bootloader2.img"
+        exit 1
+    fi
+
+    # Generate spi flash image
+    cd "${PATH_OUT}"
+ 
+    echo "${PATH_PACKAGE_TOOL}/cix_package_tool" -c "${PATH_PACKAGE_TOOL}/cix_spi_flash_config.json" -o "${PATH_OUT}/cix_flash_all.bin"
+    "${PATH_PACKAGE_TOOL}/cix_package_tool" -c "${PATH_PACKAGE_TOOL}/cix_spi_flash_config.json" -o "${PATH_OUT}/cix_flash_all.bin"
+    
+    cd -
+
+    echo -e "${GREEN}Generate ${PATH_OUT}/cix_flash_all.bin successful!${NORMAL}"
+
+}
+
+if [[ ! -e "${WORKSPACE}/edk2-non-osi/Platform/CIX/Sky1/PackageTool/build_and_package.sh" ]]; then
+    echo "Your work path ${WORKSPACE} not contain edk2-non-osi for CIX!"
+    exit 1
+fi
+
+if [[ -e "${PATH_OUT}" ]]; then
+    rm -rf "${PATH_OUT}"
+fi
+
+if [[ ! -e "${PATH_OUT}" ]]; then
+    mkdir -p "${PATH_OUT}"
+fi
+
+exec_build_uefi
+exec_cix_mkimage
